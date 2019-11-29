e167cfd7367a5b88f0f919a5eb91cb2b1ca33149
SR <sid.ravichandran@gmail.com>
2019-11-29 16:32:30 +0530

    Fixed typo in launch path for image_services

--- workers/image_services/launch.sh
+++ workers/image_services/launch.sh
@@ -5,4 +5,4 @@ echo 'Waiting for tor to start'
 
 sleep 10
 echo 'Launching image service.'
-/bin/bash -c "node ./workers/images_services/app.js"
+/bin/bash -c "node ./workers/image_services/app.js"
