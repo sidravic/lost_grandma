22a93f324bfa3d2c717037375c1dd9b4d202b01f
SR <sid.ravichandran@gmail.com>
2019-11-29 16:36:34 +0530

    Fixed launch command on staging-docker-compose.yml

--- workers/app.js
+++ workers/app.js
@@ -3,5 +3,4 @@ logger.defaultMeta.service = 'sephora-worker'
 
 const redisClient = require('./../config/redis_client');
 const {PersistQueue, PersistQueueWorker} = require('./persist_queue');
-const {ImageDownloadQueue, ImageDownloadQueueWorker} = require('./image_services/image_download_queue');
 PersistQueue.process('persist_queue', 10, PersistQueueWorker)
