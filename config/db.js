98519221561375ac644a81e2889b7f81123ab4c3
SR <sid.ravichandran@gmail.com>
2019-12-02 14:30:58 +0530

    Added postgres statement_timeouts and idle_in_session_timeouts

--- config/db.js
+++ config/db.js
@@ -3,6 +3,12 @@ const  pg = require('pg');
 delete pg.native;
 
 const sequelize = new Sequelize(process.env.DB_URL, {
+    dialectOptions: {
+      options: {
+          statement_timeout: 1000 * 60 * 3,
+          idle_in_transaction_session_timeout: 1000 * 60 * 3
+      }
+    },
     pool: {
         max: 30,
         min: 0,
